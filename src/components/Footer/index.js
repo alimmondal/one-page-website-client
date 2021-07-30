405ec72ac946f2c0d897301641a657b94b0ee59f
alimmondal <76857187+alimmondal@users.noreply.github.com>
2021-07-30 16:57:36 +0600

    updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin} from 'react-icons/fa'
 import {FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia, SocialMediaWrap, WebsiteRights, SocialIcons, SocialLogo, SocialIconLink} from './FooterElements';
+import { animateScroll as scroll } from 'react-scroll';
 
 const Footer = () => {
+
+    const toggleHome = () => {
+        scroll.scrollToTop()
+    }
 
     return (
         <FooterContainer>
@@ -44,7 +49,7 @@ const Footer = () => {
                 </FooterLinksContainer>
                 <SocialMedia>
                     <SocialMediaWrap>
-                        <SocialLogo to="/">
+                        <SocialLogo to="/" onClick={toggleHome}>
                             dolla
                         </SocialLogo>
                         <WebsiteRights>dolla &copy; {new Date().getFullYear()}  All rights reserved</WebsiteRights>
